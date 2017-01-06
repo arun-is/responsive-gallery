851332fcd64eeba40b0b3a629eb082ff77852b32
Arun Venkatesan <dallasarun@gmail.com>
2017-01-06 12:57:50 -0800

    multiple sizes
    * images now show up in multiple sizes in the grid

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 // To make images retina, add a class "2x" to the img element
 // and add a <image-name>@2x.png image. Assumes jquery is loaded.
-var dribbble_username = "ueno";
+var dribbble_username = "RypeArts";
 var queue = [];
 var page = 0;
 var done = false;
@@ -10,6 +10,25 @@ var retina;
 
 function create(el) {
   return document.createElement(el)
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
 function isRetina() {
@@ -27,17 +46,72 @@ function isRetina() {
 	return false;
 };
 
+function getImagePath(image) {
+  return image.images.hidpi || image.images.normal;
+}
+
 function setContainerWidth() {
-  $(".container").css("width", 200 * Math.floor(window.innerWidth / 200));
+  width = 400 * Math.floor(window.innerWidth / 400) || window.innerWidth;
+  $(".container").css("width", width);
+}
+
+function getSmallImage(image) {
+  return '<img class="image small" src="' + (getImagePath(image)) + '" />';
+}
+
+function addBigImage() {
+  var image = queue.shift();
+  $(".container").append('<img class="image big" src="' + (getImagePath(image)) + '" />');
+}
+
+function addSmallBlank() {
+  $(".container").append('<img class="small" src="/static/images/blank.png" />');
+}
+
+function addBigBlank() {
+  $(".container").append('<img class="big" src="/static/images/blank.png" />');
+}
+
+function addSmallImageBlock() {
+  var number = 0;
+  var random = Math.random();
+  var a = 4, b = 3, c = 2, d = 1, tot=a+b+c+d;
+  if(random < (a/tot)) {
+    number = 1;
+  } else if((a/tot) < random && random < ((a+b)/tot)) {
+    number = 2;
+  } else if((a+b)/tot < random && random < ((a+b+c)/tot)) {
+    number = 3;
+  } else if((a+b+c)/tot < random) {
+    number = 4;
+  }
+  number = Math.min(number,queue.length);
+  images = [];
+  for(var i = 0; i < number; i++) {
+    images.push(queue.shift());
+  }
+  var tags = [];
+  for(var i = 0; i < images.length; i++) {
+    tags.push(getSmallImage(images[i]));
+  }
+  for(var i = 0; i < (4 - tags.length); i++) {
+    tags.push('<img class="small" src="/static/images/blank.png" />');
+  }
+  tags = shuffle(tags);
+  var tagString = tags.join("");
+  $(".container").append('<div class="big flex flex-wrap">' + tagString + '</div>');
 }
 
 function addImage() {
-  var item = queue.shift();
-  $(".container").append('<img class="image" src="' + (retina ? item.images.hidpi : item.images.normal) + '" />');
-}
-
-function addBlank() {
-  $(".container").append('<img class="image" src="/static/images/blank.png" />')
+  var random = Math.random();
+  var a = 2, b = 1, c = 1, tot=a+b+c;
+  if(random < (a/tot)) {
+    addSmallImageBlock();
+  } else if((a/tot) < random && random < ((a+b)/tot)) {
+    addBigImage();
+  } else if(((a+b)/tot) < random) {
+    addBigBlank();
+  }
 }
 
 function shotsLoaded(shots) {
@@ -51,11 +125,8 @@ function shotsLoaded(shots) {
 
 function checkQueue() {
   if(queue.length > 0) {
-    if(Math.random() > .75) {
-      addImage();
-    } else {
-      addBlank();
-    }
+    addImage();
+
   } else {
     checkScroll();
     if(!done && spaceLeft && !wait) {
